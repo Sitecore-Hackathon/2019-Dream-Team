274b31db7e19df9faef32d9aba33291f07540ff6
Kostiantyn Tkachuk1 <kostiantyn_tkachuk1@epam.com>
2019-03-02 18:22:09 +0200

    BE / banner-component: added banner component and controllers dependency injection

---
+++ src/Foundation/DependencyInjection/code/Configurators/MvcControllerServicesConfigurator.cs
@@ -0,0 +1,22 @@
+﻿namespace DreamTeam.Foundation.DependencyInjection.Configurators
+{
+    using System;
+    using System.Linq;
+    using DreamTeam.Foundation.DependencyInjection.Extensions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.DependencyInjection;
+
+    public class MvcControllerServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(assembly => assembly.FullName.StartsWith("DreamTeam.Feature") ||
+                    assembly.FullName.StartsWith("DreamTeam.Foundation") ||
+                    assembly.FullName.StartsWith("DreamTeam.Project"));
+            serviceCollection.AddMvcControllers("DreamTeam.Feature");
+            serviceCollection.AddMvcControllers("DreamTeam.Foundation");
+            serviceCollection.AddMvcControllers("DreamTeam.Project");
+        }
+    }
+}
