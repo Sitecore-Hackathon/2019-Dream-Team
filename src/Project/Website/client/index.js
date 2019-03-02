5d5dabc8f346da4a246b5a0a8954e001964fcc00
Andrii_Shtadler <andrii_shtadler@epam.com>
2019-03-02 20:40:57 +0200

    Added ImageVision

--- src/Project/Website/client/index.js
+++ src/Project/Website/client/index.js
@@ -2,3 +2,6 @@ import 'reset-css';
 import 'axe-core/axe';
 import '../../../Feature/Banner/client';
 import '../../../Feature/Navigation/client';
+import '../../../Foundation/AccessibilityChecker/client/ImageVision';
+
+import './main.scss';
