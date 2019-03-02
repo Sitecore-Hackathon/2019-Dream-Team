3eadc5a03a01c808c3cd79d5f8590ac5e42bc542
Andrii_Shtadler <andrii_shtadler@epam.com>
2019-03-03 00:49:55 +0200

    Added WCAG2 foundation

--- src/Foundation/AccessibilityChecker/code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/AccessibilityChecker.js
+++ src/Foundation/AccessibilityChecker/code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/AccessibilityChecker.js
@@ -6,8 +6,13 @@
 
         execute: function (context) {
             ExperienceEditor.PipelinesUtil.generateRequestProcessor("DreamTeam.CheckAccessibility", function (response) {
-                var dialogFeatures = "dialogMinWidth: 850px;";
+                var dialogFeatures = "dialogMinWidth: 800px;";
                 ExperienceEditor.Dialogs.showModalDialog(response.responseValue.value, "", dialogFeatures, null);
+                axe.run(document, {
+                    exclude: ['iframe','.ui-dialog-normal']
+                }, function (data) {
+                    WCAG2.generateTable(data, document.getElementById('accessibility-result'));
+                });
             }).execute(ExperienceEditor.generatePageContext(context, ExperienceEditor.getPageEditingWindow().document));
         }
     };
