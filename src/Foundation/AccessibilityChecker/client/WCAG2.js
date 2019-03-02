7705ed6482e27bc7efe3d8b7d3f4e55d6a0a25ac
Andrii_Shtadler <andrii_shtadler@epam.com>
2019-03-03 00:38:45 +0200

    Added WCAG2 foundation

---
+++ src/Foundation/AccessibilityChecker/client/WCAG2.js
@@ -0,0 +1,37 @@
+(function(window) {
+    if (typeof window.getComputedStyle === 'function') {
+        window.WCAG2 = {
+            /**
+             *
+             * @param {Array} violations
+             */
+            generateRows: function(violations) {
+                var rows = '';
+                violations.forEach(function(item) {
+                    rows += '<tr>' +
+                        '<td><img src="/sitecore/shell/themes/standard/images/bullet_square_red.png" width="16" height="16" align="middle"\n' +
+                        '             class="" alt="" border="0"></td>' +
+                        '<td><div><b>'+ item.id +'</b> - '+ item.impact+'</div></td>' +
+                        '<td class="scValidatorResult">'+ item.description +'</td>' +
+                        '</tr>'
+                })
+
+                return rows;
+            },
+            /**
+             *
+             * @param {{violations: Array}} data
+             * @param {HTMLElement} target
+             */
+            generateTable: function (data, target) {
+                var fragment = '<table class="scListControl" width="100%" cellpadding="0" border="0">' +
+                    '<tbody>' +
+                    this.generateRows(data.violations) +
+                    '</tbody>' +
+                    '</table>';
+
+                target.innerHTML = fragment;
+            }
+        };
+    }
+})(window);
--- src/Project/Website/client/index.js
+++ src/Project/Website/client/index.js
@@ -3,5 +3,6 @@ import 'axe-core/axe';
 import '../../../Feature/Banner/client';
 import '../../../Feature/Navigation/client';
 import '../../../Foundation/AccessibilityChecker/client/ImageVision';
+import '../../../Foundation/AccessibilityChecker/client/WCAG2';
 
 import './main.scss';
