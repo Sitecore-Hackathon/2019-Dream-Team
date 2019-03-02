fa639b3a6ef7135f8dfa552cc6e749a9ae326fe7
Andrii_Shtadler <andrii_shtadler@epam.com>
2019-03-03 01:17:49 +0200

    Update axe, and WCAG2

--- src/Foundation/AccessibilityChecker/client/ImageVision.js
+++ src/Foundation/AccessibilityChecker/client/ImageVision.js
@@ -1,38 +1,20 @@
-(function(window) {
+(function (window) {
     /**
      * See how to use api on https://cloud.google.com/vision/docs
      * @param {{requests:Array<{
      *  image:{source:{imageUri: string}},
      *  features: Array
      * }>}} configs
-     * @param {boolean} isMock
      * @param {function} callback
      */
-    var imageVision = function(configs, callback, isMock) {
-        if (isMock) {
-            // returns mock data
-            var mockData = {"responses": [{"landmarkAnnotations": [{"mid": "/m/019dvv", "description": "Mount Rushmore", "score": 0.8757957, "boundingPoly": {"vertices": [{"x": 321, "y": 195}, {"x": 777, "y": 195}, {"x": 777, "y": 510}, {"x": 321, "y": 510}]}, "locations": [{"latLng": {"latitude": 43.878264, "longitude": -103.45700740814209}}]}], "webDetection": {"webEntities": [{"entityId": "/m/019dvv", "score": 255.8336, "description": "Mount Rushmore National Memorial"}, {"entityId": "/m/0373w4", "score": 12.14976, "description": "Crazy Horse Memorial"}], "fullMatchingImages": [{"url": "https://tinahanagan.files.wordpress.com/2012/04/dsc_0597.jpg"}], "partialMatchingImages": [{"url": "https://i1.wp.com/eightsails.files.wordpress.com/2016/10/img_2138.jpg"}, {"url": "https://i1.wp.com/littlezenmonkey.com/wp-content/uploads/2016/05/IMG_6994.jpg"}], "visuallySimilarImages": [{"url": "http://media.gettyimages.com/photos/the-famous-landmark-mount-rushmore-on-a-perfect-weather-day-picture-id106682515?s=170667a"}, {"url": "http://www.clevelandseniors.com/images/funny/mount-rushmore.jpg"}]}}]};
-            callback(mockData);
-            return;
-        }
-
-        var xhr = new XMLHttpRequest();
-
-        // sitecore setups key for security reason
-        xhr.open('GET', 'https://vision.googleapis.com/v1/images:annotate?key='+window.VISION_KEY, true);
-
-        // send the request to api
-        xhr.send();
-
-        xhr.onreadystatechange = function() { // (3)
-            if (xhr.readyState != 4) return;
-            if (xhr.status != 200) {
-                callback(xhr.responseText)
-            }
-        }
+    var getData = function (configs, callback) {
+        // returns mock data
+        var mockData = {"responses": [{"landmarkAnnotations": [{"mid": "/m/019dvv", "description": "Mount Rushmore", "score": 0.8757957, "boundingPoly": {"vertices": [{"x": 321, "y": 195}, {"x": 777, "y": 195}, {"x": 777, "y": 510}, {"x": 321, "y": 510}]}, "locations": [{"latLng": {"latitude": 43.878264, "longitude": -103.45700740814209}}]}], "webDetection": {"webEntities": [{"entityId": "/m/019dvv", "score": 255.8336, "description": "Mount Rushmore National Memorial"}, {"entityId": "/m/0373w4", "score": 12.14976, "description": "Crazy Horse Memorial"}], "fullMatchingImages": [{"url": "https://tinahanagan.files.wordpress.com/2012/04/dsc_0597.jpg"}], "partialMatchingImages": [{"url": "https://i1.wp.com/eightsails.files.wordpress.com/2016/10/img_2138.jpg"}, {"url": "https://i1.wp.com/littlezenmonkey.com/wp-content/uploads/2016/05/IMG_6994.jpg"}], "visuallySimilarImages": [{"url": "http://media.gettyimages.com/photos/the-famous-landmark-mount-rushmore-on-a-perfect-weather-day-picture-id106682515?s=170667a"}, {"url": "http://www.clevelandseniors.com/images/funny/mount-rushmore.jpg"}]}}]};
+        callback.call(this, mockData);
+        return;
     };
 
-    if (typeof window.getComputedStyle === 'function') {
-        window.imageVision = imageVision;
-    }
+    window.imageVision = {
+        visio: getData
+    };
 })(window);
--- src/Project/Website/client/index.js
+++ src/Project/Website/client/index.js
@@ -1,8 +1,6 @@
 import 'reset-css';
-import 'axe-core/axe';
 import '../../../Feature/Banner/client';
 import '../../../Feature/Navigation/client';
-import '../../../Foundation/AccessibilityChecker/client/ImageVision';
 import '../../../Foundation/AccessibilityChecker/client/WCAG2';
 
 import './main.scss';
