71ab0cf102f5aff185e4508054bbc687e9f0831d
Kostiantyn Tkachuk1 <kostiantyn_tkachuk1@epam.com>
2019-03-03 02:10:51 +0200

    BE / last changes

--- src/Foundation/AccessibilityChecker/code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/AccessibilityChecker.js
+++ src/Foundation/AccessibilityChecker/code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/AccessibilityChecker.js
@@ -8,10 +8,15 @@
             ExperienceEditor.PipelinesUtil.generateRequestProcessor("DreamTeam.CheckAccessibility", function (response) {
                 var dialogFeatures = "dialogMinWidth: 800px;";
                 ExperienceEditor.Dialogs.showModalDialog(response.responseValue.value, "", dialogFeatures, null);
-                axe.run(document, {
-                    exclude: ['iframe','.ui-dialog-normal']
+
+                var parent = ExperienceEditor.getPageEditingWindow();
+
+                console.log(window.axe);
+
+                window.axe.run(parent.document, {
+                    //exclude: ['iframe','.ui-dialog-normal']
                 }, function (data) {
-                    WCAG2.generateTable(data, document.getElementById('accessibility-result'));
+                    window.WCAG2.generateTable(data, document.getElementById('accessibility-result'));
                 });
             }).execute(ExperienceEditor.generatePageContext(context, ExperienceEditor.getPageEditingWindow().document));
         }
